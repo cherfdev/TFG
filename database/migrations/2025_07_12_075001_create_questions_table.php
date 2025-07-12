b8a9d96d2b4d41162a5da6228c8e863873cabb6a
unknown <yaroucherifkoto@gmail.com>
2025-07-12 08:56:35 +0100

    commit 1

---
+++ database/migrations/2025_07_12_075001_create_questions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('intitule');
+            $table->string('categorie')->nullable();
+            $table->integer('index_in_categorie')->default(0);
+            $table->boolean('obligatoire')->default(false);
+            $table->foreignId('type_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('questions');
+    }
+};
